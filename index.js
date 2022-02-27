7411c64399cd053ddf6340d0a4e2a9e187a7791d
Trey Herrera <therrera152@gmail.com>
2022-02-27 09:58:58 -0600

    commit tree f0c34bf7e290a9ca0585854d3e1f2d75cc03711f

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (num1, num2) => 2000/100;
+
+
+const square = (x) => x * x;
+
+const add = (a,b) => a + b;
+
+
+// I have ran all three of these tests on J Fiddle and they
+// all give me the right answers. 
